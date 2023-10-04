295a0a8b693a969c3735c64bc39039af5965e8c5
Cassy Dalil <cassydalil@gmail.com>
2023-10-03 21:47:20 -0400

    herosection.js and css

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,7 +1,19 @@
-function Main() {
+import React from "react";
+import "../components/styles/herosection.css";
+import RestaurantFood from "./assets/restauranfood.jpg";
+
+function HeroSection() {
     return (
-      <main></main>
+      <div className="hero-section-container">
+        <div className="hero-section-text-container">
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+          <button>Book a Table</button>
+        </div>
+        <img className="restaurant-food-img" src={RestaurantFood} alt="restaurant food"></img>
+      </div>
     );
   }
 
-export default Main;
+export default HeroSection;
