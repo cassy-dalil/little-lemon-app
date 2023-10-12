71f97edac886f9560d1564ee77eb5a15f4d6676f
Cassy Dalil <cassydalil@gmail.com>
2023-10-11 22:03:54 -0400

    hero-section css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     return (
       <footer>
         <img className="footer-img-logo" src={footerLogo} alt="Little Lemon logo"></img>
-        <div className="footer-navigation">
+        <div className="footer-navigation footer-div">
             <li className="footer-nav-list-item list-item">Home</li>
             <li className="footer-nav-list-item list-item">About</li>
             <li className="footer-nav-list-item list-item">Menu</li>
@@ -14,16 +14,16 @@ function Footer() {
             <li className="footer-nav-list-item list-item">Order Online</li>
             <li className="footer-nav-list-item list-item">Login</li>
         </div>
-        <div className="footer-contact">
-          <p>Contact</p>
+        <div className="footer-contact footer-div">
+          <h6 className="footer-p">Contact</h6>
           <ul className="footer-contact-list">
             <li className="footer-contact-list-item list-item">Address</li>
             <li className="footer-contact-list-item list-item">Phone number</li>
             <li className="footer-contact-list-item list-item">Email</li>
           </ul>
         </div>
-        <div className="footer-social-media">
-          <p>Social Media Links</p>
+        <div className="footer-social-media footer-div">
+          <h6 className="footer-p">Social Media Links</h6>
           <ul className="footer-social-media-list">
             <li className="footer-social-media-list-item list-item">Media link 1</li>
             <li className="footer-social-media-list-item list-item">Media link 2</li>
