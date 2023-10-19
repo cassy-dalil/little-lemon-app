39c1e40cc6e5b09a8c5944d7a42736c377dcc3fb
Cassy Dalil <cassydalil@gmail.com>
2023-10-18 21:42:02 -0400

    added more to specials.css

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -12,21 +12,21 @@ function Specials() {
                 <button className="specials-order-online-button">Order Online</button>
             </article>
             <section className="specials-cards-container">
-                <div className="greek-salad-card">
+                <div className="greek-salad-card specials-card">
                     <img className="greek-salad-img specials-img" src={GreekSalad} alt="Greek Salad"></img>
                     <h3 className="greek-salad-title specials-title">Greek Salad</h3>
                     <h3 className="greek-salad-price specials-price">$12.99</h3>
                     <p className="greek-salad-description specials-description">Refreshing salad, made with tomato, lettuce, feta cheese, and olives. Dressed with salt, hot pepper, and olive oil.</p>
                     <button className="order-delivery-button">Order for delivery</button>
                 </div>
-                <div className="bruschetta-card">
+                <div className="bruschetta-card specials-card">
                     <img className="bruschetta-img specials-img" src={Bruschetta} alt="Bruschetta"></img>
                     <h3 className="bruschetta-title specials-title">Bruschetta</h3>
                     <h3 className="bruschetta-price specials-price">$12.99</h3>
                     <p className="bruschetta-description specials-description">Toasted bread, topped with tomato, prosciutto, and cheese. Seasoned with salt and olive oil.</p>
                     <button className="order-delivery-button">Order for delivery</button>
                 </div>
-                <div className="lemon-dessert-card">
+                <div className="lemon-dessert-card specials-card">
                     <img className="lemon-dessert-img specials-img" src={LemonDessert} alt="Lemon Dessert"></img>
                     <h3 className="lemon-dessert-title specials-title">Lemon Dessert</h3>
                     <h3 className="lemon-dessert-price specials-price">$12.99</h3>
