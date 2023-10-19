a15a1d7437e1d1804b247693e462e48914bf42a9
Cassy Dalil <cassydalil@gmail.com>
2023-10-18 22:40:53 -0400

    added testimonials.js and css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import HeroSection from './components/HeroSection.js';
 import Specials from './components/Specials';
 import Footer from './components/Footer.js';
+import Testimonials from './components/Testimonials';
 
 // import {Route, Routes} from "react-route-dom";
 
@@ -14,6 +15,7 @@ function App() {
         <Header />
         <HeroSection />
         <Specials />
+        <Testimonials />
         <Footer />
       </React.Fragment>
       {/* <Routes> 
---
+++ src/components/Testimonials.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/testimonials.css";
+
+function Testimonials() {
+    return(
+        <section className="testimonials-container">
+            <h2 className="testimonials-title">Testimonials</h2>
+        </section>
+    )
+}
+
+export default Testimonials;
