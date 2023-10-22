127bb804ebe4e255ce30726f39cd43648a515ddb
Cassy Dalil <cassydalil@gmail.com>
2023-10-21 22:57:48 -0400

    testimonials text and css

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -5,6 +5,24 @@ function Testimonials() {
     return(
         <section className="testimonials-container">
             <h2 className="testimonials-title">Testimonials</h2>
+            <section className="testimonials-text">
+                <div className="testimonial-card">
+                    <p className="testimonial-message">This is the best Mediterranean food that I've ever had!</p>
+                    <p className="testimonial-name">- Michael Scott</p>
+                </div>
+                <div className="testimonial-card">
+                    <p className="testimonial-message">The food here is delicious and fresh!</p>
+                    <p className="testimonial-name">- Jimmy John</p>
+                </div>
+                <div className="testimonial-card">
+                    <p className="testimonial-message">My family loves dining in the Little Lemon restaurant because of the tasty food and great service.</p>
+                    <p className="testimonial-name">- Nancy Drew</p>
+                </div>
+                <div className="testimonial-card">
+                    <p className="testimonial-message">The entire menu here is fire! Def would recommend to anybody!</p>
+                    <p className="testimonial-name">- Zach Long</p>
+                </div>
+            </section>
         </section>
     )
 }
