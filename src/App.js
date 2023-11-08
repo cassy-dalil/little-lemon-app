6374ac6a49e841f3409925f6af22c424a4e5d9de
Cassy Dalil <cassydalil@gmail.com>
2023-11-07 21:31:06 -0500

    fixed router

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/reservations" element={<Reservations />}></Route>
+        <Route path="/" element={<Home />}/>
+        <Route path="/reservations" element={<Reservations />}/>
       </Routes>
     </>
   );
