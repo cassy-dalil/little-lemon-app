0e238bf226a3874cb3398d8cf39ddfd9290ada11
Cassy Dalil <cassydalil@gmail.com>
2023-11-26 15:27:21 -0500

    fixed link in specials.js

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -18,21 +18,21 @@ function Specials() {
                     <h3 className="greek-salad-title specials-title">Greek Salad</h3>
                     <h3 className="greek-salad-price specials-price">$12.99</h3>
                     <p className="greek-salad-description specials-description">Refreshing salad, made with tomato, lettuce, feta cheese, and olives. Dressed with salt, hot pepper, and olive oil.</p>
-                    <Link to="orderonline" className="order-delivery-button">Order for delivery</Link>
+                    <Link to="/orderonline" className="order-delivery-button">Order for delivery</Link>
                 </div>
                 <div className="bruschetta-card specials-card">
                     <img className="bruschetta-img specials-img" src={Bruschetta} alt="Bruschetta"></img>
                     <h3 className="bruschetta-title specials-title">Bruschetta</h3>
                     <h3 className="bruschetta-price specials-price">$12.99</h3>
                     <p className="bruschetta-description specials-description">Toasted bread, topped with tomato, prosciutto, and cheese. Seasoned with salt and olive oil.</p>
-                    <Link to="orderonline" className="order-delivery-button">Order for delivery</Link>
+                    <Link to="/orderonline" className="order-delivery-button">Order for delivery</Link>
                 </div>
                 <div className="lemon-dessert-card specials-card">
                     <img className="lemon-dessert-img specials-img" src={LemonDessert} alt="Lemon Dessert"></img>
                     <h3 className="lemon-dessert-title specials-title">Lemon Dessert</h3>
                     <h3 className="lemon-dessert-price specials-price">$12.99</h3>
                     <p className="lemon-dessert-description specials-description">Fresh baked lemon bread coated in salt and sugar. Powdered in citrus and lemon zest.</p>
-                    <Link to="orderonline" className="order-delivery-button">Order for delivery</Link>
+                    <Link to="/orderonline" className="order-delivery-button">Order for delivery</Link>
                 </div>
             </section>
         </section>
